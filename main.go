9d95581e4b1b2561ff8e17f8ce93dfc27a3b9dfb
Твалавадзе Алексей Борисович <alexey.tvalavadze@goods.ru>
2022-02-21 23:38:07 +0300

    test3

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	solution "github.com/golang-united-school-homework-1/solution"
+)
+
+func main() {
+	fmt.Println(solution.GetMessage())
 }
---
+++ solution/solution.go
@@ -0,0 +1,7 @@
+package solution
+
+import emoji "github.com/kyokomi/emoji"
+
+func GetMessage() string {
+	return emoji.Sprint("Hello :world_map:!")
+}
